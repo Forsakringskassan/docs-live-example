1e148feb3437f15caab6f4a001dc797d4a7b0744
David Sveningsson <david.sveningsson@forsakringskassan.se>
2024-04-13 10:23:28 +0200

    fix: updated language detection

--- src/LiveExample.vue
+++ src/LiveExample.vue
@@ -59,7 +59,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { type PropType, defineComponent } from "vue";
 import { highlight, stripComments } from "./utils";
 import LiveVueCode from "./live-vue-code";
 import { type ExpandAnimation, expandAnimation } from "./expand-animation";
@@ -70,6 +70,27 @@ export default defineComponent({
     name: "LiveExample",
     components: { LiveVueCode },
     props: {
+        /**
+         * Explicitly render example in given language.
+         *
+         * Must be one of:
+         *
+         * - `"vue"` - Interpret `template` as a Vue SFC.
+         * - `"html"` - Interpret `template` as vanilla HTML.
+         *
+         * Default is `"auto"` but you should not explicitly set this value
+         * yourself. When set to `"auto"` the contents of `template` prop will
+         * be autodetected based on some heurestics (subject to change at any
+         * time).
+         */
+        language: {
+            type: String as PropType<"vue" | "html" | "auto">,
+            required: false,
+            default: "auto",
+            validator(value: string): boolean {
+                return ["vue", "html", "auto"].includes(value);
+            },
+        },
         template: {
             type: String,
             required: true,
@@ -98,6 +119,7 @@ export default defineComponent({
     },
     data() {
         return {
+            parentLanguage: "",
             idPrefix: `live-example-${idCounter++}`,
             codeLanguage: "html",
             codeExpand: {
@@ -109,6 +131,25 @@ export default defineComponent({
         };
     },
     computed: {
+        computedLanguage(): "vue" | "html" {
+            /* explicit language set */
+            if (this.language !== "auto") {
+                return this.language;
+            }
+
+            /* autodetect by explicit language attribute on parent */
+            if (this.parentLanguage) {
+                /* we are lying slightly here, we are only supposed to use these
+                two languages but the code will still work even if not: we
+                should maybe declare it as "string" instead though */
+                return this.parentLanguage as "html" | "vue";
+            }
+
+            /* autodetect by legacy method (i.e. it is vue code only when it
+             * passes child components) */
+            const hasChildComponents = Object.keys(this.components).length > 0;
+            return hasChildComponents ? "vue" : "html";
+        },
         isVue(): boolean {
             return Object.keys(this.components).length > 0;
         },
@@ -143,6 +184,10 @@ export default defineComponent({
         },
     },
     mounted() {
+        const parent = this.$el.closest("[data-language]");
+        if (parent) {
+            this.parentLanguage = parent.dataset.language ?? "";
+        }
         if (this.isVue) {
             this.codeLanguage = "vue";
         }
