2d32053ef50e120efa6df678efe294bf8c1098ef
David Sveningsson <david.sveningsson@forsakringskassan.se>
2026-07-04 19:18:27 +0200

    chore(deps): update @forsakringskassan/eslint-config monorepo to v15.2.0

--- src/create-element.spec.ts
+++ src/create-element.spec.ts
@@ -1,3 +1,4 @@
+import { expect, it } from "@jest/globals";
 import { createElement } from "./create-element";
 
 it("should serialize element", () => {
--- src/utils/highlight.spec.ts
+++ src/utils/highlight.spec.ts
@@ -1,3 +1,4 @@
+import { expect, it } from "@jest/globals";
 import { highlight } from "./highlight";
 
 it("should be formatted and highlighted", async () => {
